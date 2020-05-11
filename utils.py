8d5998808439cba1ee58a19225f3c6cff3294368
xzhou29 <xin9186@gmail.com>
2020-05-11 14:26:39 -0500

    changed to absolute path

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ def get_files(this_path):
     results = []
     arr = os.listdir(this_path)
     for a in arr:
-        results.append(this_path + "/" + a)
+        temp_path = this_path + "/" + a
+        final_path = os.path.abspath(temp_path)
+        results.append(final_path)
+
     return results
 
